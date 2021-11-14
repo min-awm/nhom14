9b8b7bf9426572035fe6dae2577f50f78b4eeb3d
minha05112003a <73045983+minha05112003a@users.noreply.github.com>
2021-11-14 13:17:42 +0700

    Add files via upload

---
+++ precache-manifest.16cd7c03a1dbd8679051b2fb76036c58.js
@@ -0,0 +1,79 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a94d14881cde1ac3cccca8a233504d7",
+    "url": "/app.html"
+  },
+  {
+    "revision": "4394d10d6fbb10996dd1",
+    "url": "/css/app.c1a5f151.css"
+  },
+  {
+    "revision": "880d5d9b71b0309163ca6b238aa67d71",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "ca8ae55106ab4c797b42d5b23724bf42",
+    "url": "/favicon.svg"
+  },
+  {
+    "revision": "98ed3b081a5a0e0d9570cbed91b82689",
+    "url": "/favicon16.png"
+  },
+  {
+    "revision": "4e098d27c1d58a36046424c6bf0e7cf6",
+    "url": "/favicon32.png"
+  },
+  {
+    "url": "/font\\Ubuntu-Light.eot"
+  },
+  {
+    "url": "/font\\Ubuntu-Light.svg"
+  },
+  {
+    "url": "/font\\Ubuntu-Light.ttf"
+  },
+  {
+    "url": "/font\\Ubuntu-Light.woff"
+  },
+  {
+    "url": "/font\\Ubuntu-Light.woff2"
+  },
+  {
+    "url": "/font\\Ubuntu-Regular.eot"
+  },
+  {
+    "url": "/font\\Ubuntu-Regular.svg"
+  },
+  {
+    "url": "/font\\Ubuntu-Regular.ttf"
+  },
+  {
+    "url": "/font\\Ubuntu-Regular.woff"
+  },
+  {
+    "url": "/font\\Ubuntu-Regular.woff2"
+  },
+  {
+    "url": "/fonts.css"
+  },
+  {
+    "revision": "384e3fe8db9354371f55",
+    "url": "/js/about.9e194c37.js"
+  },
+  {
+    "revision": "4394d10d6fbb10996dd1",
+    "url": "/js/app.c9696751.js"
+  },
+  {
+    "revision": "df1301dec208e11deef6",
+    "url": "/js/chunk-vendors.0d69cbb8.js"
+  },
+  {
+    "revision": "b9a6fbd46b05781594b4ffc429faab1f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "50d8a018e8ae96732c8a2ba663c61d4e",
+    "url": "/robots.txt"
+  }
+]);
